7c44fb5bd5168498576ddb41fa0260680377c516
Santiago Castillo <castillograndasantiagoeduardo@gmail.com>
2026-04-27 14:33:24 -0500

    Login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login_v2._0.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Esta acción solo muestra la vista
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginVistas.Models // O el namespace que estés usando actualmente
+{
+    public class LoginViewModel
+    {
+        public string Documento { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+    }
+}
